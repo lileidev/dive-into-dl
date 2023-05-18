71e1144805a1eabe45102cb36271dfe53f096bc1
lilei <li_leibj@inspur.com>
2023-05-18 18:17:32 +0800

    update

--- d2lzh_pytorch.py
+++ d2lzh_pytorch.py
@@ -1,7 +1,13 @@
 import random
+import sys
 from IPython import display
 from matplotlib import pyplot as plt
 import torch
+import torchvision
+import torchvision.transforms as transforms
+
+import matplotlib
+matplotlib.use('TkAgg')
 
 def use_svg_display():
     display.set_matplotlib_formats('svg')
@@ -33,3 +39,36 @@ def get_fashion_mnist_labels(labels):
     text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat', 'sandal', 'shirt', 
                    'sneaker', 'bag', 'ankle boot']
     return [text_labels[int(i)] for i in labels]
+
+
+def show_fashion_mnist(images, labels):
+    use_svg_display()
+    _, figs = plt.subplots(1, len(images), figsize=(12, 12))
+    for f, img, lbl in zip(figs, images, labels):
+        f.imshow(img.view((28, 28)).numpy())
+        f.set_title(lbl)
+        f.axes.get_xaxis().set_visible(False)
+        f.axes.get_yaxis().set_visible(False)
+    plt.show()
+
+def load_data_fashion_mnist(batch_size):
+    batch_size = 256
+    if sys.platform.startswith('win'):
+        num_workers = 0
+    else:
+        num_workers = 4
+
+    mnist_train = torchvision.datasets.FashionMNIST(root='./Datasets/FashionMNIST', train=True, download=False, transform=transforms.ToTensor())
+    mnist_test = torchvision.datasets.FashionMNIST(root='./Datasets/FashionMNIST', train=False, download=False, transform=transforms.ToTensor())
+
+
+    train_iter = torch.utils.data.DataLoader(mnist_train,
+                                            batch_size=batch_size, 
+                                            shuffle=True, 
+                                            num_workers=num_workers)
+    test_iter = torch.utils.data.DataLoader(mnist_test, 
+                                            batch_size=batch_size,
+                                            shuffle=False,
+                                            num_workers=num_workers)
+    
+    return train_iter, test_iter
--- softmax_reg.py
+++ softmax_reg.py
@@ -1,11 +1,27 @@
 import torch
-import torchvision
-import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
-import time
-import sys
 import d2lzh_pytorch as d2l
+import numpy as np
 
-mnist_train = torchvision.datasets.FashionMNIST(root='./Datasets/FashionMNIST', train=True, download=True, transform=transforms.ToTensor())
-mnist_test = torchvision.datasets.FashionMNIST(root='./Datasets/FashionMNIST', train=False, download=True, transform=transforms.ToTensor())
+batch_size = 256
+train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size)
+
+num_inputs = 784
+num_outputs = 10
+
+W = torch.tensor(np.random.normal(0, 0.01, (num_inputs, num_outputs)), dtype=torch.float)
+b = torch.zeros(num_outputs, dtype=torch.float)
+
+W.requires_grad_(requires_grad=True)
+b.requires_grad_(requires_grad=True)
+
+def softmax(X):
+    X_exp = X.exp()
+    partition = X_exp.sum(dim=1, keepdim=True)
+    return X_exp / partition
+
+def num(X):
+    return softmax(torch.mm(X.view((-1, num_inputs)), W) + b)
+
+def cross_entropy(y_hat, y):
+    return - torch.log(y_hat.gather(1, y.view(-1, 1)))
 
